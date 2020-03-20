47b6940890febfbe80bd05802cb554e2c9df7219
Matt Kuras <mattkuras@gmail.com>
2020-03-20 21:32:28 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  #your code here
+  
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+attributes = {:name => "matt", :hair_color => "brown", :eye_color => "green", :handed =>"right"}
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
 end
