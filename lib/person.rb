70dd343027af2d710d00a1ac90c38951230dacae
Matt Kuras <mattkuras@gmail.com>
2020-03-20 21:27:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,9 +4,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-attributes = {:name => "matt", :birthday, :hair_color =>, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width}
+attributes = {:name => "matt", :hair_color => "brown", :eye_color => "green", :handed = >"right"}
   
   def initialize(hash)
 end
